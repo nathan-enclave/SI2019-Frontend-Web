0500910196d1ed41adb991955c7f9cf1a9dfa7b8
= <=>
2019-07-19 15:21:20 +0700

    base crud config

--- src/components/Presentation/pages/engineers/index/TableData.js
+++ src/components/Presentation/pages/engineers/index/TableData.js
@@ -31,11 +31,6 @@ class TableData extends Component {
     this.componentWillMount();
   }
   async componentWillMount(){
-    // let offset = ((this.state.activePage)*(this.state.itemsCountPerPage))
-    // console.log(offset)
-    // const res = await getDataPag(this.state.itemsCountPerPage,offset);
-    // this.setState({totalItemsCount : res.total})
-    const res0 = await getData();
     this.setState({totalItemsCount : res0.total})
     // console.log("active page: " + this.state.activePage)
     let offset = ((this.state.activePage)*(this.state.itemsCountPerPage))
