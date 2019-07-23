7410e2ab1c7dcf9d8405d6455f92bc2ffa4ee7b6
= <=>
2019-07-23 10:08:55 +0700

    applying base CRUD to engnineer and fix inserting engineer

--- src/components/Presentation/commons/msg/MSGSuccess.js
+++ src/components/Presentation/commons/msg/MSGSuccess.js
@@ -5,15 +5,10 @@ export default class MSGSuccess extends Component {
     
     return (
       <div className="DeletePopUp">
-        {/* <div class="modal-content"> */}
           <div className="modal-header">
               <h4 className="modal-title">Message</h4>
           </div>
-          <div className="modal-body">Update profile successfully.</div>
-          {/* <div class="modal-footer">
-              <button type="button" onClick={this.props.onClose} class="btn red">OK</button>
-          </div> */}
-      {/* </div> */}
+          <div className="modal-body">{this.props.message}</div>
       </div>
     )
   }
