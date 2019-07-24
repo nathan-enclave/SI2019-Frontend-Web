e1f8bb35eb4a8baeb773a7df03bd5f8ebd068d0b
Trang <tranginspirit78@gmail.com>
2019-07-24 23:42:35 +0700

    fix color

--- src/components/presentation/pages/project/viewProject/view/ViewProject.js
+++ src/components/presentation/pages/project/viewProject/view/ViewProject.js
@@ -69,6 +69,23 @@ class EditForm extends Component {
     }
   }
   render() {
+    let root = document.documentElement;
+    if (this.state.status === "done") {
+      root.style.setProperty('--bg', "#B5CEFD")
+      root.style.setProperty('--border', "#AEBBDD")
+      root.style.setProperty('--boxColor', "#659be0")
+    }
+    if (this.state.status === "pending") {
+      root.style.setProperty('--bg', "#FFFFA8")
+      root.style.setProperty('--border', "#FFFF00")
+      root.style.setProperty('--boxColor', "#F1C40F")
+    }
+    if (this.state.status === "inProgress") {
+      root.style.setProperty('--bg', "#B8E7F5")
+      root.style.setProperty('--border', "#8CFFFF")
+      root.style.setProperty('--boxColor', "#36c6d3")
+    }
+
     let color = null
     if (this.state.status === "done") {
       color = 'label-info'
@@ -91,10 +108,10 @@ class EditForm extends Component {
           <div className="portlet-title tabbable-line">
             <div className="caption">
               <i className=" icon-social-twitter font-dark hide" />
-             <Link to={"/teams/" + this.state.teamId} className={"label label-sm label-default"} style={{ fontSize: "15px" }}> {this.state.team} </Link>
+              <Link to={"/teams/" + this.state.teamId} className={"label label-sm label-default"} style={{ fontSize: "15px" }}> {this.state.team} </Link>
             </div>
           </div>
-          <div className="portlet-body4">
+          <div className="portlet-body-custom-color">
             <div className="tab-content">
               <div className="tab-pane active" id="tab_actions_pending">
                 {this.state.teamData}
@@ -104,90 +121,97 @@ class EditForm extends Component {
         </div>
       )
     let loadData = (this.state.data !== null) ? (
-      <div className="portlet-body">
-        <div className="row">
-          <div className="col-lg-6 col-xs-12 col-sm-12">
-            <div className="portlet light bordered">
-              <div className="portlet-title tabbable-line">
-                <div className="caption">
-                  <i className="icon-bubbles font-dark hide" />
-                  <span className="caption-subject font-dark bold uppercase">BASIC INFORMATION</span>
-                </div>
-              </div>
-              <div className="portlet-body3" >
-                <div className="tab-content">
-                  <div className="portlet-body">
-                    <div className="general-item-list">
-                      <div className="item">
-                        <div className="item-head">
-                          <div className="item-details">
-                            <span className="item-name" >Project name</span>
-                          </div>
-                        </div>
-                        <div className="mt-comment-text"> {this.state.name}    </div>
-                      </div>
-                      <div className="item">
-                        <div className="item-head">
-                          <div className="item-details">
-                            <span className="item-name">Status</span>
-                          </div>
-                        </div>
-                        <div className="mt-comment-text">  <span className={"label label-sm " + color} style={{ fontSize: "15px" }}> {this.state.status} </span>   </div>
-                      </div>
-                      <div className="item">
-                        <div className="item-head">
-                          <div className="item-details">
-                            <span className="item-name">Category</span>
-                          </div>
-                        </div>
-                        <div className="mt-comment-text"> {this.state.category}    </div>
-                      </div>
-                      <div className="item">
-                        <div className="item-head">
-                          <div className="item-details">
-                            <span className="item-name">Description</span>
-                          </div>
-                        </div>
-                        <div className="mt-comment-text"> {this.state.description}    </div>
-                      </div>
-                      <div className="item">
-                        <div className="item-head">
-                          <div className="item-details">
-                            <span className="item-name">Technology</span>
-                          </div>
-                        </div>
-                        <div className="mt-comment-text"> {this.state.technology}    </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+      <div className="portlet box custom color">
+        <div className="portlet-title">
+          <div className="caption">
+            {this.state.name}   </div>
+        </div>
+        <div className="portlet-body">
+          <div className="row">
+            <div className="col-lg-6 col-xs-12 col-sm-12">
               <div className="portlet light bordered">
                 <div className="portlet-title tabbable-line">
                   <div className="caption">
-                    <i className=" icon-social-twitter font-dark hide" />
-                    <span className="caption-subject font-dark bold uppercase">finance</span>
+                    <i className="icon-bubbles font-dark hide" />
+                    <span className="caption-subject font-dark bold uppercase">BASIC INFORMATION</span>
                   </div>
                 </div>
-                <div className="portlet-body">
+                <div className="portlet-body-custom-color">
                   <div className="tab-content">
-                    <div className="table-main-pagination">
-                      <div className="table-scrollable">
-                        <table className="table table-striped table-bordered table-advance table-hover">
-                          <thead>
-                            <tr>
-                              <th >Earning </th>
-                              <th>Earning Per Month </th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                              <th >{this.state.earningPerMonth} </th>                         
-                              <th >{this.state.earning} </th>
-                          </tbody>
-                        </table>
+                    <div className="portlet-body">
+                      <div className="general-item-list">
+                        <div className="item">
+                          <div className="item-head">
+                            <div className="item-details">
+                              <span className="item-name" >Project name</span>
+                            </div>
+                          </div>
+                          <div className="mt-comment-text"> {this.state.name}    </div>
+                        </div>
+                        <div className="item">
+                          <div className="item-head">
+                            <div className="item-details">
+                              <span className="item-name">Status</span>
+                            </div>
+                          </div>
+                          <div className="mt-comment-text">  <span className={"label label-sm " + color} style={{ fontSize: "15px" }}> {this.state.status} </span>   </div>
+                        </div>
+                        <div className="item">
+                          <div className="item-head">
+                            <div className="item-details">
+                              <span className="item-name">Category</span>
+                            </div>
+                          </div>
+                          <div className="mt-comment-text"> {this.state.category}    </div>
+                        </div>
+                        <div className="item">
+                          <div className="item-head">
+                            <div className="item-details">
+                              <span className="item-name">Description</span>
+                            </div>
+                          </div>
+                          <div className="mt-comment-text"> {this.state.description}    </div>
+                        </div>
+                        <div className="item">
+                          <div className="item-head">
+                            <div className="item-details">
+                              <span className="item-name">Technology</span>
+                            </div>
+                          </div>
+                          <div className="mt-comment-text"> {this.state.technology}    </div>
+                        </div>
                       </div>
                     </div>
                   </div>
+                </div>
+                <div className="portlet light bordered">
+                  <div className="portlet-title tabbable-line">
+                    <div className="caption">
+                      <i className=" icon-social-twitter font-dark hide" />
+                      <span className="caption-subject font-dark bold uppercase">finance</span>
+                    </div>
+                  </div>
+                  <div className="portlet-bodyx">
+                    <div className="tab-content">
+                      <div className="table-main-pagination">
+                        <div className="table-scrollable-custom">
+                          <table className="table table-striped table-bordered table-advance table-hover">
+                            <thead>
+                              <tr>
+                                <th width="50%">Earning </th>
+                                <th>Earning Per Month </th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <tr>
+                                <th width="50%">{this.state.earningPerMonth} VND</th>
+                                <th >{this.state.earning} VND</th>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                   <div className="portlet light bordered">
                     <div className="portlet-title tabbable-line">
@@ -196,26 +220,24 @@ class EditForm extends Component {
                         <span className="caption-subject font-dark bold uppercase">TIME</span>
                       </div>
                     </div>
-                    <div className="portlet-body3" >
+                    <div className="portlet-bodyx">
                       <div className="tab-content">
-                        <div className="portlet-body">
-                          <div className="general-item-list">
-                            <div className="item-block">
-                              <div className="item-head">
-                                <div className="item-details">
-                                  <span className="item-name" >Start</span>
-                                </div>
-                              </div>
-                              <div className="mt-comment-text"> {this.state.start}    </div>
-                            </div>
-                            <div className="item-last">
-                              <div className="mt-comment">
-                                <div className="item-details">
-                                  <span className="item-name">End</span>
-                                </div>
-                              </div>
-                              <div className="mt-comment-text"> {this.state.end}    </div>
-                            </div>
+                        <div className="table-main-pagination">
+                          <div className="table-scrollable-custom">
+                            <table className="table table-striped table-bordered table-advance table-hover">
+                              <thead>
+                                <tr>
+                                  <th width="50%">Start </th>
+                                  <th>End </th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                <tr>
+                                  <th width="50%">{this.state.start} </th>
+                                  <th >{this.state.end} </th>
+                                </tr>
+                              </tbody>
+                            </table>
                           </div>
                         </div>
                       </div>
@@ -232,7 +254,7 @@ class EditForm extends Component {
                     <span className="caption-subject font-dark bold uppercase">ratio</span>
                   </div>
                 </div>
-                <div className="portlet-body">
+                <div className="portlet-bodyx">
                   <div className="tab-content">
                     <div>
                       <Chart options={this.state.options} series={this.state.series} type="pie" width="70%" />
@@ -244,17 +266,13 @@ class EditForm extends Component {
             </div>
           </div>
         </div>
-        ) : <Preloader styleCustom={"unset"} />
-        return (
+      </div>
+    ) : <Preloader styleCustom={"unset"} />
+    return (
       <div className="portlet light bordered">
-          <div className="portlet red box">
-            <div className="portlet-title">
-              <div className="caption">
-                {this.state.name}   </div>
-            </div>
-            {loadData}
-          </div>
-        </div>)
-    }
+        {loadData}
+      </div>
+    )
   }
+}
 export default EditForm;
