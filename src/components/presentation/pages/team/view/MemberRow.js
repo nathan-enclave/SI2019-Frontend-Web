d98ec89eb300571894c4a452cf057efca32c140a
Trang <tranginspirit78@gmail.com>
2019-07-24 08:42:06 +0700

    fix project layout + validate form

--- src/components/presentation/pages/team/view/MemberRow.js
+++ src/components/presentation/pages/team/view/MemberRow.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
-
-class MemberRow extends Component {
-    constructor(props) {
-        super(props)
-    }
+class MemberRow extends Component {    
     render() {
         let typeLabel = (this.props.role === "leader") ? "danger" : "success"
         return (
