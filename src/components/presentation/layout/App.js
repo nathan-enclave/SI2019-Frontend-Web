9c31cebdfd0a9fc1ef388302ef0958a3f40de137
Trang <tranginspirit78@gmail.com>
2019-08-08 08:32:11 +0700

    fix router

--- src/components/presentation/layout/App.js
+++ src/components/presentation/layout/App.js
@@ -1,32 +1,40 @@
 import React, { Component } from 'react'
 import RouterURL from '../router/RouterURL';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from './Header';
+import Login from '../pages/login/Login'
 import LeftBar from './LeftBar';
 import Footer from './Footer';
-import {Redirect } from 'react-router'
+import ForgetPW from '../pages/login/ForgetPW';
+import ResetPassword from '../pages/login/ResetPassword';
+import ResetSuccess from '../pages/login/ResetSuccess';
+import { Redirect } from 'react-router'
 import Notification from "../include/Notification";
 import { SnackbarProvider } from 'notistack';
-export default class App extends Component {   
-    render() {            
-        if (!localStorage.getItem('sessionToken')) {        
+
+export default class App extends Component {
+    render() {
+        if (!localStorage.getItem('sessionToken')) {
             return (
                 <div >
                     <Router>
-                    <RouterURL />
-                    <Redirect to ="/login" />
+                        <Route path="/forgotPassword" component={ForgetPW} />
+                        <Route path="/resetPassword" component={ResetPassword} />
+                        <Route path="/resetSuccess" component={ResetSuccess} />
+                        <Route exact path="/login" component={Login} />
+                        <Redirect to="/login" />
                     </Router>
                 </div>
             );
         }
         else {
-            return (               
-                <SnackbarProvider maxSnack={3}>                    
-                    <Notification/>
-                    <div className="App">                       
+            return (
+                <SnackbarProvider maxSnack={3}>
+                    <Notification />
+                    <div className="App">
                         <Router>
                             <div className="container-fluid">
-                                <div className="page-content page-content-popup">      
+                                <div className="page-content page-content-popup">
                                     <Header />
                                     <div className="page-content-fixed-header">
                                         <ul className="page-breadcrumb">
@@ -35,14 +43,14 @@ export default class App extends Component {
                                         <div className="content-header-menu">
                                             <button type="button" className="menu-toggler responsive-togngler" data-toggle="collapse" data-target=".navbar-collapse">
                                                 <span className="toggle-icon toggle-icon-custom">
-                                                <i className="fa fa-bars"></i>
+                                                    <i className="fa fa-bars"></i>
                                                 </span>
                                             </button>
                                         </div>
                                     </div>
                                     <LeftBar />
-                                    <div className="page-fixed-main-content">                                   
-                                        <RouterURL />                                        
+                                    <div className="page-fixed-main-content">
+                                        <RouterURL />
                                         <div className="col-md-6">
                                         </div>
                                         <div className="clearfix"></div>
@@ -51,9 +59,9 @@ export default class App extends Component {
                                 </div>
                             </div>
                             <div className="quick-nav-overlay"></div>
-                        </Router>                      
-                    </div> 
-                </SnackbarProvider>                    
+                        </Router>
+                    </div>
+                </SnackbarProvider>
             );
         }
     }
